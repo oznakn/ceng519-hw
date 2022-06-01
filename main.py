7be0b6c9fac93f29d888be8a5a1934e93d0f485b
Ozan Akın <e2309599@ceng.metu.edu.tr>
2022-06-01 14:04:03 +0000

    improvements

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from eva.ckks import CKKSCompiler
 from eva.seal import generate_keys
 from eva.metric import valuation_mse
 
+ctx = dict()
+
 def generate_graph(n: int, k: int, p: int):
     # ws = nx.cycle_graph(n)
     G = nx.watts_strogatz_graph(n, k, p)
@@ -27,23 +29,54 @@ def serialize_graph(G, vec_size):
 
 # Eva requires special input, this function prepares the eva input
 # Eva will then encrypt them
-def prepare_input(n, vec_size):
+def prepare_initial_input(n, vec_size):
     input = {}
 
     G = generate_graph(n, 3, 0.5)
 
     edge_list = serialize_graph(G, vec_size)
-    input['Edges'] = edge_list
+    input["Edges"] = edge_list
 
     return input
 
-def graph_boruvka(graph, graph_size):
-    reval = 0
+def set_context(node_count, edge_count):
+    ctx["node_count"] = node_count
+    ctx["edge_count"] = edge_count
 
-    for i in range(graph_size):
-        reval += pow(graph<<i, 2)
 
-    return reval
+def pre_compute_parent(parent):
+    result = []
+
+    for u in range(len(parent)):
+        while parent[u] != u:
+            u = parent[u]
+        result.append(u)
+
+    return result
+
+def action_union(computed_parent, parent, rank, x, y):
+    x_root = computed_parent[x]
+    y_root = computed_parent[y]
+
+    if x_root != y_root:
+        if rank[x_root] < rank[y_root]:
+            parent[x_root] = y_root
+        elif rank[x_root] > rank[y_root]:
+            parent[y_root] = x_root
+        else:
+            parent[y_root] = x_root
+            rank[x_root] += 1
+
+def graph_boruvka_1(edges):
+    print("hey")
+    node_count = ctx["node_count"]
+    edge_count = ctx["edge_count"]
+
+    cheapest = [-1 for _ in range(node_count)]
+
+    for i in range(0, edge_count, 3):
+        return (0, 0)
+
 
 # Do not change this
 # the parameter n can be passed in the call from simulate function
@@ -58,83 +91,62 @@ class EvaProgramDriver(EvaProgram):
     def __exit__(self, exc_type, exc_value, traceback):
         super().__exit__(exc_type, exc_value, traceback)
 
+def step(public_ctx, secret_ctx, signature, compiled_func, inputs):
+    encInputs = public_ctx.encrypt(inputs, signature)
+    encOutputs = public_ctx.execute(compiled_func, encInputs)
+
+    outputs = secret_ctx.decrypt(encOutputs, signature)
+
+    reference = evaluate(compiled_func, inputs)
+
+    # Change this if you want to output something or comment out the two lines below
+    for key in outputs:
+        print(key, float(outputs[key][0]), float(reference[key][0]))
+
+
 # Repeat the experiments and show averages with confidence intervals
 # You can modify the input parameters
 # n is the number of nodes in your graph
 # If you require additional parameters, add them
 def simulate(n):
     m = 4096*4
-
     print("Will start simulation for ", n)
+
+    inputs = prepare_initial_input(n, m)
+
+    set_context(n, len(inputs["Edges"]))
+
     config = {}
     config['warn_vec_size'] = 'false'
     config['lazy_relinearize'] = 'true'
     config['rescaler'] = 'always'
     config['balance_reductions'] = 'true'
-    inputs = prepare_input(n, m)
 
-    eva_prog = EvaProgramDriver("graph_boruvka", vec_size=m, n=n)
-    with eva_prog:
-        graph = Input('Edges')
-        reval = graph_boruvka(graph, n)
-        Output('ReturnedValue', reval)
-
-    eva_prog.set_output_ranges(30)
-    eva_prog.set_input_scales(30)
-
-    start = timeit.default_timer()
     compiler = CKKSCompiler(config=config)
-    compiled_multfunc, params, signature = compiler.compile(eva_prog)
-    compiletime = (timeit.default_timer() - start) * 1000.0 #ms
 
-    start = timeit.default_timer()
+    eva_prog_1 = EvaProgramDriver("graph_boruvka", vec_size=m, n=n)
+    with eva_prog_1:
+        edges = Input('Edges')
+        reval, x = graph_boruvka_1(edges)
+
+        Output('ReturnedValue', reval)
+        Output('X', x)
+
+    eva_prog_1.set_output_ranges(30)
+    eva_prog_1.set_input_scales(30)
+
+    compiled_func_1, params, signature = compiler.compile(eva_prog_1)
     public_ctx, secret_ctx = generate_keys(params)
-    keygenerationtime = (timeit.default_timer() - start) * 1000.0 #ms
 
-    start = timeit.default_timer()
-    encInputs = public_ctx.encrypt(inputs, signature)
-    encryptiontime = (timeit.default_timer() - start) * 1000.0 #ms
-
-    start = timeit.default_timer()
-    encOutputs = public_ctx.execute(compiled_multfunc, encInputs)
-    executiontime = (timeit.default_timer() - start) * 1000.0 #ms
-
-    start = timeit.default_timer()
-    outputs = secret_ctx.decrypt(encOutputs, signature)
-    decryptiontime = (timeit.default_timer() - start) * 1000.0 #ms
-
-    start = timeit.default_timer()
-    reference = evaluate(compiled_multfunc, inputs)
-    referenceexecutiontime = (timeit.default_timer() - start) * 1000.0 #ms
-
-    # Change this if you want to output something or comment out the two lines below
-    for key in outputs:
-        print(key, float(outputs[key][0]), float(reference[key][0]))
-
-    mse = valuation_mse(outputs, reference) # since CKKS does approximate computations, this is an important measure that depicts the amount of error
-
-    return compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse
+    step(public_ctx, secret_ctx, signature, compiled_func_1, inputs)
 
 
 if __name__ == "__main__":
-    simcnt = 3 #The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
-    # For benchmarking you must set it to a large number, e.g., 100
-    #Note that file is opened in append mode, previous results will be kept in the file
-    resultfile = open("results.csv", "a")  # Measurement results are collated in this file for you to plot later on
-    resultfile.write("NodeCount,SimCnt,CompileTime,KeyGenerationTime,EncryptionTime,ExecutionTime,DecryptionTime,ReferenceExecutionTime,Mse\n")
-    resultfile.close()
+    num_sim = 1 # The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
 
     print("Simulation campaing started:")
-    for nc in range(36,64,4): # Node counts for experimenting various graph sizes
-        n = nc
-        resultfile = open("results.csv", "a")
-        for i in range(simcnt):
-            #Call the simulator
-            compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse = simulate(n)
-            res = str(n) + "," + str(i) + "," + str(compiletime) + "," + str(keygenerationtime) + "," +  str(encryptiontime) + "," +  str(executiontime) + "," +  str(decryptiontime) + "," +  str(referenceexecutiontime) + "," +  str(mse) + "\n"
-            print(res)
-            # resultfile.write(res)
-            break # TODO: remove
-        break # TODO: remove
 
-        resultfile.close()
+    for n in range(36,64,4): # Node counts for experimenting various graph sizes
+        for i in range(num_sim):
+            simulate(n)
+        break # TODO: remove
