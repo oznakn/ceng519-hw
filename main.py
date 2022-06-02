64d9e09363fbb2686295b4282d7ff2ded95e8ff1
Ozan Akın <e2309599@ceng.metu.edu.tr>
2022-06-02 11:08:02 +0000

    impl

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from eva.ckks import CKKSCompiler
 from eva.seal import generate_keys
 from eva.metric import valuation_mse
 
-ctx = dict()
-
 def generate_graph(n: int, k: int, p: int):
     # ws = nx.cycle_graph(n)
     G = nx.watts_strogatz_graph(n, k, p)
@@ -27,30 +25,16 @@ def serialize_graph(G, vec_size):
 
     return edge_list
 
-# Eva requires special input, this function prepares the eva input
-# Eva will then encrypt them
-def prepare_initial_input(n, vec_size):
-    input = {}
-
-    G = generate_graph(n, 3, 0.5)
-
-    edge_list = serialize_graph(G, vec_size)
-    input["Edges"] = edge_list
-
-    return input
-
-def set_context(node_count, edge_count):
-    ctx["node_count"] = node_count
-    ctx["edge_count"] = edge_count
-
-
-def pre_compute_parent(parent):
+def pre_compute_parent(parent, vec_size):
     result = []
 
     for u in range(len(parent)):
         while parent[u] != u:
             u = parent[u]
         result.append(u)
+
+    for _ in range(vec_size - len(result)):
+        result.append(0)
 
     return result
 
@@ -67,29 +51,19 @@ def action_union(computed_parent, parent, rank, x, y):
             parent[y_root] = x_root
             rank[x_root] += 1
 
-def graph_boruvka_1(edges):
-    print("hey")
-    node_count = ctx["node_count"]
-    edge_count = ctx["edge_count"]
-
+def graph_boruvka_1(node_count, edge_count, data):
     cheapest = [-1 for _ in range(node_count)]
 
     for i in range(0, edge_count, 3):
-        return (0, 0)
+        u = data << i
+        v = data << (i + 1)
+        w = data << (i + 2)
 
+        set1 = data << (edge_count + u)
+        set2 = data << (edge_count + v)
 
-# Do not change this
-# the parameter n can be passed in the call from simulate function
-class EvaProgramDriver(EvaProgram):
-    def __init__(self, name, vec_size=4096, n=4):
-        self.n = n
-        super().__init__(name, vec_size)
+        return (set1, set2)
 
-    def __enter__(self):
-        super().__enter__()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        super().__exit__(exc_type, exc_value, traceback)
 
 def step(public_ctx, secret_ctx, signature, compiled_func, inputs):
     encInputs = public_ctx.encrypt(inputs, signature)
@@ -108,26 +82,29 @@ def step(public_ctx, secret_ctx, signature, compiled_func, inputs):
 # You can modify the input parameters
 # n is the number of nodes in your graph
 # If you require additional parameters, add them
-def simulate(n):
-    m = 4096*4
-    print("Will start simulation for ", n)
-
-    inputs = prepare_initial_input(n, m)
-
-    set_context(n, len(inputs["Edges"]))
+def simulate(node_count):
+    print("Will start simulation for ", node_count)
 
     config = {}
-    config['warn_vec_size'] = 'false'
+    config['warn_vec_size'] = 'true'
     config['lazy_relinearize'] = 'true'
     config['rescaler'] = 'always'
     config['balance_reductions'] = 'true'
 
     compiler = CKKSCompiler(config=config)
 
-    eva_prog_1 = EvaProgramDriver("graph_boruvka", vec_size=m, n=n)
+    vec_size = 4096
+
+    G = generate_graph(node_count, 3, 0.5)
+    edge_list = serialize_graph(G, vec_size // 2)
+
+    edge_count = len(edge_list)
+    parent = [i for i in range(node_count)]
+
+    eva_prog_1 = EvaProgram("graph_boruvka_1", vec_size=vec_size)
     with eva_prog_1:
-        edges = Input('Edges')
-        reval, x = graph_boruvka_1(edges)
+        data = Input('data')
+        reval, x = graph_boruvka_1(node_count, edge_count, data)
 
         Output('ReturnedValue', reval)
         Output('X', x)
@@ -138,13 +115,17 @@ def simulate(n):
     compiled_func_1, params, signature = compiler.compile(eva_prog_1)
     public_ctx, secret_ctx = generate_keys(params)
 
-    step(public_ctx, secret_ctx, signature, compiled_func_1, inputs)
+    while True:
+        inputs = {"data": edge_list + pre_compute_parent(parent, vec_size // 2)}
+
+        step(public_ctx, secret_ctx, signature, compiled_func_1, inputs)
+        break
 
 
 if __name__ == "__main__":
     num_sim = 1 # The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
 
-    print("Simulation campaing started:")
+    print("Simulation started:")
 
     for n in range(36,64,4): # Node counts for experimenting various graph sizes
         for i in range(num_sim):
