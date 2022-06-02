c7cc1863632ad803ba5c5e4984e2cea59aa7e50b
Ozan Akın <e2309599@ceng.metu.edu.tr>
2022-06-02 11:38:27 +0000

    impl

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ from eva.ckks import CKKSCompiler
 from eva.seal import generate_keys
 from eva.metric import valuation_mse
 
+VEC_SIZE = 4096
+
+compiler_config = {}
+compiler_config['warn_vec_size'] = 'true'
+compiler_config['lazy_relinearize'] = 'true'
+compiler_config['rescaler'] = 'always'
+compiler_config['balance_reductions'] = 'true'
+
+compiler = CKKSCompiler(config=compiler_config)
+
 def generate_graph(n: int, k: int, p: int):
     # ws = nx.cycle_graph(n)
     G = nx.watts_strogatz_graph(n, k, p)
 
     return G
 
-def serialize_graph(G, vec_size):
+def serialize_graph(G):
     node_count = G.size()
     total_edge_count = 0
 
@@ -24,22 +34,17 @@ def serialize_graph(G, vec_size):
                 adj_matrix[row*node_count + column] = 1
                 total_edge_count += 1
 
-    adj_matrix += [0 for _ in range(vec_size - len(adj_matrix))]
-
     print('created a graph with total edge count: ', total_edge_count)
 
     return adj_matrix
 
-def pre_compute_parent(parent, vec_size):
+def pre_compute_parent(parent):
     result = []
 
     for u in range(len(parent)):
         while parent[u] != u:
             u = parent[u]
         result.append(u)
-
-    for _ in range(vec_size - len(result)):
-        result.append(0)
 
     return result
 
@@ -58,9 +63,8 @@ def action_union(computed_parent, parent, rank, x, y):
 
 def graph_boruvka_1(node_count, data):
     parent_data_head = node_count*node_count
-    cheapest = [-1 for _ in range(node_count)]
 
-    result = 0
+    result = []
 
     for u in range(node_count):
         for v in range(node_count):
@@ -72,7 +76,7 @@ def graph_boruvka_1(node_count, data):
             set1 = data << (parent_data_head + u)
             set2 = data << (parent_data_head + v)
 
-            result += w
+            result.append((set1, set2, w))
 
     return result
 
@@ -89,34 +93,16 @@ def step(public_ctx, secret_ctx, signature, compiled_func, inputs):
         print(key, float(outputs[key][0]), float(reference[key][0]))
 
 
-# Repeat the experiments and show averages with confidence intervals
-# You can modify the input parameters
-# n is the number of nodes in your graph
-# If you require additional parameters, add them
-def simulate(node_count):
-    print("Will start simulation for ", node_count)
-
-    config = {}
-    config['warn_vec_size'] = 'true'
-    config['lazy_relinearize'] = 'true'
-    config['rescaler'] = 'always'
-    config['balance_reductions'] = 'true'
-
-    compiler = CKKSCompiler(config=config)
-
-    vec_size = 4096
-
-    G = generate_graph(node_count, 3, 0.5)
-    adj_matrix = serialize_graph(G, vec_size // 2)
-
-    parent = [i for i in range(node_count)]
-
-    eva_prog_1 = EvaProgram("graph_boruvka_1", vec_size=vec_size)
+def prepare_simulation(node_count):
+    eva_prog_1 = EvaProgram("graph_boruvka_1", vec_size=VEC_SIZE)
     with eva_prog_1:
         data = Input('data')
-        reval = graph_boruvka_1(node_count, data)
+        result = graph_boruvka_1(node_count, data)
 
-        Output('ReturnedValue', reval)
+        for i in range(len(result)):
+            Output(f"Result_{i}_s1", result[i][0])
+            Output(f"Result_{i}_s2", result[i][1])
+            Output(f"Result_{i}_w", result[i][2])
 
     eva_prog_1.set_output_ranges(30)
     eva_prog_1.set_input_scales(30)
@@ -124,10 +110,28 @@ def simulate(node_count):
     compiled_func_1, params, signature = compiler.compile(eva_prog_1)
     public_ctx, secret_ctx = generate_keys(params)
 
-    while True:
-        inputs = {"data": adj_matrix + pre_compute_parent(parent, vec_size // 2)}
+    return compiled_func_1, public_ctx, secret_ctx, signature
 
-        step(public_ctx, secret_ctx, signature, compiled_func_1, inputs)
+# Repeat the experiments and show averages with confidence intervals
+# You can modify the input parameters
+# n is the number of nodes in your graph
+# If you require additional parameters, add them
+def simulate(node_count):
+    print("Will start simulation for ", node_count)
+
+    compiled_func_1, public_ctx, secret_ctx, signature = prepare_simulation(node_count)
+
+    G = generate_graph(node_count, 3, 0.5)
+    adj_matrix = serialize_graph(G)
+
+    parent = [i for i in range(node_count)]
+    cheapest = [-1 for _ in range(node_count)]
+
+    while True:
+        data = adj_matrix + pre_compute_parent(parent) + cheapest
+        data += [0 for _ in range(VEC_SIZE - len(data))]
+
+        step(public_ctx, secret_ctx, signature, compiled_func_1, {"data": data})
         break
 
 
@@ -135,8 +139,8 @@ if __name__ == "__main__":
     num_sim = 1 # The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
 
     print("Simulation started:")
+    simulate(4)
 
-    for n in range(36,64,4): # Node counts for experimenting various graph sizes
-        for i in range(num_sim):
-            simulate(n)
-        break # TODO: remove
+    # for n in range(36,64,4): # Node counts for experimenting various graph sizes
+    #     for i in range(num_sim):
+    #     break # TODO: remove
